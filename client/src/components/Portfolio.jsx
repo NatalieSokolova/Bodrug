75ae2e9abf2a00d493d2df2559de22a7e76ff13c
NatalieSokolova <nataliesklv@gmail.com>
2020-10-16 23:50:52 +0000

    adds links to menu items

---
+++ client/src/components/Portfolio.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function Phortfolio({}) {
+  return (
+    <div>
+      <h1>Photo Portfolio</h1>
+    </div>
+  );
+}
