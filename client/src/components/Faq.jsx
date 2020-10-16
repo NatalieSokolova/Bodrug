86650c026edccf3d20795f8d76e69c2324f2bf2a
NatalieSokolova <nataliesklv@gmail.com>
2020-10-16 20:56:13 +0000

    adds frontend routes

---
+++ client/src/components/Faq.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function Faq({}) {
+  return (
+    <div>
+      <h1>These are FAQs</h1>
+      <h2>Question</h2>
+      <p>Answer</p>
+    </div>
+  );
+}
