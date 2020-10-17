23653fb83e4a818c7862cccc17f6c4dbea01e6a2
NatalieSokolova <nataliesklv@gmail.com>
2020-10-17 03:53:08 +0000

    adds contact form

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import EmailForm from "./EmailForm";
 
 export default function Contact({}) {
   return (
     <div>
-      <h1>email</h1>
+      <h1>email me:</h1>
+      <div>
+        <EmailForm />
+      </div>
       <h1>phone??</h1>
       <h1>social media?</h1>
       <h1>something else</h1>
