2edf0a584b3d91a636cc083aac530da6ef562f80
NatalieSokolova <nataliesklv@gmail.com>
2020-10-17 01:50:43 +0000

    creates a hook to fetch photo data

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import usePhotoData from "../hooks/usePhotoData";
 
 export default function Phortfolio({}) {
+  const { state, setState } = usePhotoData();
+
+  const photoList = state.photos.map((photo) => (
+    <li>
+      <div>
+        <img src={`../assets/photos/${photo.url}`} />
+      </div>
+      <div>{photo.description}</div>
+    </li>
+  ));
+
   return (
     <div>
       <h1>Photo Portfolio</h1>
+      <div>{photoList}</div>
     </div>
   );
 }
