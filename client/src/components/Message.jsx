03438023656c6cf25ad725b989da5d495ed78628
NatalieSokolova <nataliesklv@gmail.com>
2020-10-19 22:56:23 +0000

    sets state for message components

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { Component, useState, useEffect } from "react";
 import useMessageData from "../hooks/useMessageData";
-
 import { Form, Input, Button } from "antd";
 
 const layout = {
@@ -11,6 +10,7 @@ const layout = {
     span: 16,
   },
 };
+
 const validateMessages = {
   required: "${label} is required!",
   types: {
@@ -22,61 +22,73 @@ const validateMessages = {
   },
 };
 
-const Message = () => {
-  const { state, setState } = useMessageData();
+class Message extends Component {
+  // const { state, setState } = useMessageData();
 
-  const onFinish = (values) => {
-    console.log(values);
+  state = {
+    name: "",
+    email: "",
+    message: "",
+    sent: false,
+    buttonText: "Send Message",
   };
 
-  return (
-    <Form
-      {...layout}
-      name="nest-messages"
-      onFinish={onFinish}
-      validateMessages={validateMessages}
-    >
-      <Form.Item
-        name={["name"]}
-        label="Name"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
+  render() {
+    const onFinish = (values) => {
+      console.log(values);
+    };
+
+    console.log("THIS: ", this);
+
+    return (
+      <Form
+        {...layout}
+        name="nest-messages"
+        onFinish={onFinish}
+        validateMessages={validateMessages}
       >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name={["email"]}
-        label="Email"
-        rules={[
-          {
-            type: "email",
-            required: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name={["message"]}
-        label="Message"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input.TextArea />
-      </Form.Item>
-      <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
-  );
-};
+        <Form.Item
+          name={["name"]}
+          label="Name"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name={["email"]}
+          label="Email"
+          rules={[
+            {
+              type: "email",
+              required: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name={["message"]}
+          label="Message"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input.TextArea />
+        </Form.Item>
+        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  }
+}
 
 export default Message;
