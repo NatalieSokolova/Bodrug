67dfa3036bc99e9441fcd47b0c10114c8b2e6a8d
NatalieSokolova <nataliesklv@gmail.com>
2020-10-20 00:02:54 +0000

    changes Message component

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+// const bodyParser = require("body-parser");
+// const nodemailer = require("nodemailer");
+// const cors = require("cors");
+
 module.exports = (db) => {
   // GET messages listing.
   router.get("/", (req, res) => {
@@ -14,19 +18,47 @@ module.exports = (db) => {
   });
 
   //test
-  router.post("/messages", (req, res, next) => {
+  router.post("/messages", (req, res) => {
     // create an insert query in the db
-    const query = {
-      text: `INSERT INTO messages (name, email, message) VALUES ($1, $2, $3) RETURNING *;`,
-      values: [name, email, message],
+    //   const query = {
+    //     text: `INSERT INTO messages (name, email, message) VALUES ($1, $2, $3) RETURNING *;`,
+    //     values: [name, email, message],
+    //   };
+
+    //   db.query(query)
+    //     .then((result) => res.json(result[0]))
+    //     .catch((err) => console.log(err));
+
+    //   // return the newly created message back
+    // });
+    var data = req.body;
+
+    var smtpTransport = nodemailer.createTransport({
+      service: "yahoo",
+      port: 465,
+      auth: {
+        user: "testforbodrug@yahoo.com",
+        pass: "yManiutin19?o",
+      },
+    });
+
+    var mailOptions = {
+      from: data.email,
+      to: "testforbodrug@yahoo.com",
+      subject: "New Message For Bodrug",
+      html: `<p>${data.name}</p>
+            <p>${data.email}</p>
+            <p>${data.message}</p>`,
     };
 
-    db.query(query)
-      .then((result) => res.json(result[0]))
-      .catch((err) => console.log(err));
-
-    // return the newly created message back
+    smtpTransport.sendMail(mailOptions, (error, res) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send("Success");
+      }
+      smtpTransport.close();
+    });
   });
-
   return router;
 };
--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,134 +1,76 @@
-import React, { Component, useState, useEffect } from "react";
-import axios from "axios";
-import useMessageData from "../hooks/useMessageData";
-import { Form, Input, Button } from "antd";
-
-const layout = {
-  labelCol: {
-    span: 8,
-  },
-  wrapperCol: {
-    span: 16,
-  },
-};
-
-const validateMessages = {
-  required: "${label} is required!",
-  types: {
-    email: "${label} is not validate email!",
-    number: "${label} is not a validate number!",
-  },
-  number: {
-    range: "${label} must be between ${min} and ${max}",
-  },
-};
-
-class Message extends Component {
-  // const { state, setState } = useMessageData();
-
-  state = {
-    name: "",
-    email: "",
-    message: "",
-    sent: false,
-    buttonText: "Send Message",
-  };
-
-  resetForm = () => {
-    this.setState({
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       name: "",
-      message: "",
       email: "",
-      buttonText: "Message Sent",
-    });
-  };
-
-  formSubmit = (e) => {
-    e.preventDefault();
-
-    this.setState({
-      buttonText: "...sending",
-    });
-
-    let data = {
-      name: this.state.name,
-      email: this.state.email,
-      message: this.state.message,
+      feedback: "",
     };
-
-    axios
-      .post("API_URI", data)
-      .then((res) => {
-        this.setState({ sent: true }, this.resetForm());
-      })
-      .catch(() => {
-        console.log("Message not sent");
-      });
-  };
+  }
+  handleInputChange(event) {
+    event.preventDefault();
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+    this.setState({ [name]: value });
+  }
 
   render() {
-    const onFinish = (values) => {
-      console.log(values);
-    };
-
-    console.log("THIS: ", this);
-
     return (
-      <Form
-        {...layout}
-        name="nest-messages"
-        onFinish={onFinish}
-        validateMessages={validateMessages}
-        onSubmit={(e) => {
-          this.formSubmit(e);
-          console.log("SUBMITTED");
-        }}
-      >
-        <Form.Item
-          name={["name"]}
-          label="Name"
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-          onChange={(e) => this.setState({ name: e.target.value })}
+      <div>
+        <form
+          className="ui form"
+          id={this.props.id}
+          name={this.props.name}
+          method={this.props.method}
+          action={this.props.action}
         >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name={["email"]}
-          label="Email"
-          rules={[
-            {
-              type: "email",
-              required: true,
-            },
-          ]}
-          onChange={(e) => this.setState({ email: e.target.value })}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name={["message"]}
-          label="Message"
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-          onChange={(e) => this.setState({ message: e.target.value })}
-        >
-          <Input.TextArea />
-        </Form.Item>
-        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-          <Button type="primary" htmlType="submit">
-            {this.state.buttonText}
-          </Button>
-        </Form.Item>
-      </Form>
+          <textarea
+            id="name"
+            name="name"
+            onChange={this.handleInputChange.bind(this)}
+            placeholder="your name"
+            required
+            value={this.state.name}
+            style={{ width: "100%" }}
+            rows={1}
+          />
+          <br />
+          <textarea
+            id="email"
+            name="email"
+            onChange={this.handleInputChange.bind(this)}
+            placeholder="your email address"
+            required
+            value={this.state.email}
+            error={this.state.errors.email}
+            style={{ width: "100%" }}
+            rows={1}
+          />
+          <br />
+          <textarea
+            id="feedback"
+            name="feedback"
+            onChange={this.handleInputChange.bind(this)}
+            placeholder="what would you like to chat about?"
+            required
+            value={this.state.feedback}
+            style={{ width: "100%", height: "250px" }}
+          />
+          <br />
+          <input
+            type="button"
+            value="Send"
+            className="ui button"
+            style={{
+              fontFamily: "Amatic SC",
+              fontSize: "20px",
+              color: "blue",
+            }}
+            onClick={this.sendMessage.bind(this)}
+          />
+        </form>
+      </div>
     );
   }
 }
-
-export default Message;
