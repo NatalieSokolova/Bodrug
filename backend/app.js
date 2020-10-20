1732f2929419fa58bb0a84a1ba86b084aa268cdd
NatalieSokolova <nataliesklv@gmail.com>
2020-10-20 00:02:54 +0000

    changes Message component

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,9 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const db = require("./db");
 
-// var cors = require("cors");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 // const usersRouter = require("./routes/users");
@@ -20,7 +22,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-// app.use(cors());
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
