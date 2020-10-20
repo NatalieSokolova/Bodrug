8348a72e9a945c3f9a4a60f3f50e7097655cdc3e
NatalieSokolova <nataliesklv@gmail.com>
2020-10-20 20:45:56 +0000

    uses useForm to validate form on submit

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,39 +1,44 @@
 import React from "react";
 import { useAlert } from "react-alert";
-import { positions, transitions, types } from "react-alert";
+import { useForm } from "react-hook-form";
 import emailjs from "emailjs-com";
+import "./Message.css";
 
 export default function Message() {
+  const { register, handleSubmit, errors } = useForm();
   const alert = useAlert();
 
   const sendEmail = (e) => {
-    e.preventDefault();
+    // e.preventDefault();
+
+    var templateParams = {
+      name: "Natalie",
+      notes: "Check this out!",
+    };
 
     emailjs
-      .sendForm(
+      .send(
         "service_mwelobp",
         "template_oyae051",
-        e.target,
+        templateParams,
         "user_mOdU1UDxNSuDbnFQZ9Qh6"
       )
-      .then(
-        (result) => {
-          console.log(result.text);
-          alert.show("Thank you! We'll get in touch with you shortly", {
-            type: "success",
-          });
-        },
-        (error) => {
-          console.log(error.text);
-          alert.show("Sorry, your email was not sent. Please try again", {
-            type: "error",
-          });
-        }
-      );
+      .then((result) => {
+        console.log(result.text);
+        alert.show("Thank you! We'll get in touch with you shortly", {
+          type: "success",
+        });
+      })
+      .catch((e) => {
+        console.log("TROUBLE: ", e.text);
+        alert.show("Sorry, your email was not sent. Please try again", {
+          type: "error",
+        });
+      });
   };
 
   return (
-    <form onSubmit={sendEmail}>
+    <form onSubmit={handleSubmit(sendEmail)}>
       <div className="form-group">
         <label htmlFor="name">Name</label>
         <input
@@ -41,7 +46,10 @@ export default function Message() {
           name="name"
           className="form-control"
           placeholder="Enter Your Name"
+          ref={register({ required: true })}
+          // errors will return when field validation fails
         />
+        {errors.name && <span id="error">Please, input your name</span>}
       </div>
       <div className="form-group">
         <label htmlFor="email">Email</label>
@@ -49,8 +57,18 @@ export default function Message() {
           type="email"
           name="email"
           className="form-control"
-          placeholder="Enter Your Email"
+          placeholder="Enter Your Name"
+          ref={register({
+            required: true,
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "Please, Enter A Valid Email Address",
+            },
+          })}
         />
+        {errors.email && (
+          <span id="error">Please, Enter A Valid Email Address</span>
+        )}
       </div>
       <div className="form-group">
         <label htmlFor="message">Message</label>
@@ -59,7 +77,12 @@ export default function Message() {
           name="message"
           className="form-control"
           placeholder="Enter Your Message"
+          ref={register({ required: true })}
+          // errors will return when field validation fails
         />
+        {errors.message && (
+          <span id="error">Sorry, you can't submil an empty message</span>
+        )}
       </div>
 
       <button type="submit" className="btn btn-primary">
