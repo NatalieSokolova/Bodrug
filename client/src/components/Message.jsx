f48d215525dedaa168a15fab9004649fc887cb03
NatalieSokolova <nataliesklv@gmail.com>
2020-10-20 23:23:25 +0000

    fixes email content

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,13 +1,5 @@
-// import React, { useEffect } from "react";
-// import { useAlert } from "react-alert";
-// import { useForm } from "react-hook-form";
-// import emailjs from "emailjs-com";
-// import "./Message.css";
-
 import React, { Component } from "react";
-import toastr from "toastr";
 import * as emailjs from "emailjs-com";
-import "jquery";
 import "./Message.css";
 
 class Message extends Component {
@@ -34,9 +26,10 @@ class Message extends Component {
     this.setState({ [name]: value });
 
     console.log(this.state);
+    console.log(target);
   };
 
-  validateMail = () => {
+  validateMail() {
     let errors = {};
     let formIsValid = true;
 
@@ -45,7 +38,7 @@ class Message extends Component {
       formIsValid = false;
     }
 
-    const pattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    let pattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
     if (!pattern.test(this.state.email)) {
       errors.email = "Please, enter a valid email";
       formIsValid = false;
@@ -61,9 +54,9 @@ class Message extends Component {
     }
 
     return formIsValid;
-  };
+  }
 
-  sendEmail = (event) => {
+  sendEmail(event) {
     event.preventDefault();
 
     if (!this.validateMail()) {
@@ -71,9 +64,8 @@ class Message extends Component {
     }
 
     const templateParams = {
-      from_name: `${this.state.name} (${this.state.email})`,
-      to_name: "Yuliia Bodrug",
-      message_html: this.state.message,
+      name: `${this.state.name} (${this.state.email})`,
+      message: this.state.message,
     };
 
     emailjs
@@ -85,13 +77,17 @@ class Message extends Component {
       )
       .then(
         (result) => {
-          toastr.success(
+          alert(
             "Your message was sent successfully! We'll get back to you soon"
           );
+          // toastr.success(
+          //   "Your message was sent successfully! We'll get back to you soon"
+          // );
           console.log("YAY!", result.status, result.text);
         },
         (error) => {
-          toastr.error(error);
+          // toastr.error(error);
+          alert("Sorry, your email was not sent. Please, try again");
           console.log("TROUBLE! ", error);
         }
       );
@@ -101,7 +97,7 @@ class Message extends Component {
       email: "",
       message: "",
     });
-  };
+  }
 
   render() {
     return (
@@ -119,6 +115,10 @@ class Message extends Component {
             name="name"
             className="form-control"
             placeholder="Enter Your Name"
+            required="required"
+            onChange={this.handleInputChange.bind(this)}
+            value={this.state.name}
+            error={this.state.errors.name}
           />
         </div>
         <div className="form-group">
@@ -128,6 +128,10 @@ class Message extends Component {
             name="email"
             className="form-control"
             placeholder="Enter Your Email"
+            required="required"
+            onChange={this.handleInputChange.bind(this)}
+            value={this.state.email}
+            error={this.state.errors.email}
           />
         </div>
         <div className="form-group">
@@ -137,9 +141,19 @@ class Message extends Component {
             name="message"
             className="form-control"
             placeholder="Enter Your Message"
+            required="required"
+            onChange={this.handleInputChange.bind(this)}
+            value={this.state.message}
+            error={this.state.errors.message}
           />
         </div>
-        <button type="submit" className="btn btn-primary">
+        <button
+          type="button"
+          name="submit"
+          className="btn btn-primary"
+          required="required"
+          onClick={this.sendEmail.bind(this)}
+        >
           Submit
         </button>
       </form>
