feacc77430c335757a9f9457bd5387485e384ea9
NatalieSokolova <nataliesklv@gmail.com>
2020-10-20 02:05:05 +0000

    changes emailjs template to specify sender's name and email

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 import emailjs from "emailjs-com";
+import { Form, Input, InputNumber, Button } from "antd";
 
 export default function Message() {
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
+
+  // const validateMessages = {
+  //   required: "${label} is required!",
+  //   types: {
+  //     email: "${label} is not validate email!",
+  //     number: "${label} is not a validate number!",
+  //   },
+  //   number: {
+  //     range: "${label} must be between ${min} and ${max}",
+  //   },
+  // };
+
+  // const onFinish = (values) => {
+  //   console.log(values);
+  // };
+
+  // this stays
   function sendEmail(e) {
     e.preventDefault();
 
@@ -26,12 +48,45 @@ export default function Message() {
     <form className="contact-form" onSubmit={sendEmail}>
       <input type="hidden" name="contact_number" />
       <label>Name</label>
-      <input type="text" name="user_name" />
+      <input type="text" name="name" />
       <label>Email</label>
-      <input type="email" name="user_email" />
+      <input type="email" name="email" />
       <label>Message</label>
       <textarea name="message" />
       <input type="submit" value="Send" />
     </form>
+
+    // <Form
+    //   {...layout}
+    //   name="nest-messages"
+    //   onFinish={onFinish}
+    //   validateMessages={validateMessages}
+    //   onSubmit={sendEmail}
+    // >
+    //   <Form.Item name={"name"} label="Name" rules={[{ required: true }]}>
+    //     <Input />
+    //   </Form.Item>
+    //   <Form.Item
+    //     name={"email"}
+    //     label="Email"
+    //     rules={[{ type: "email", required: true }]}
+    //   >
+    //     <Input />
+    //   </Form.Item>
+    //   <Form.Item
+    //     name={"message"}
+    //     label="message"
+    //     rules={[{ required: true }]}
+    //     type="submit"
+    //     value="Send"
+    //   >
+    //     <Input.TextArea />
+    //   </Form.Item>
+    //   <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+    //     <Button type="submit" htmlType="submit">
+    //       Submit
+    //     </Button>
+    //   </Form.Item>
+    // </Form>
   );
 }
