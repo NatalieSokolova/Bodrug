be55f0c0de15add6099e3af9e49bc98deeddd4e8
Natalie Sokolova <nataliesklv@gmail.com>
2020-10-23 20:03:29 -0400

    adds images to home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,27 @@
 import React from "react";
+import "./Home.css";
 
 export default function Home({}) {
-  return <h1>THIS IS THE HOME PAGE</h1>;
+  return (
+    <div>
+      <span id="photo1-home">
+        <img
+          src={require("../assets/photos/2-min.jpg")}
+          alt="by Yuliia Bodrug"
+        />
+      </span>
+      <span id="painting-home">
+        <img
+          src={require("../assets/paintings/sergio-souza-gOzD9CZTwXw-unsplash.jpg")}
+          alt="by Yuliia Bodrug"
+        />
+      </span>
+      <span id="photo2-home">
+        <img
+          src={require("../assets/photos/15-min.jpg")}
+          alt="by Yuliia Bodrug"
+        />
+      </span>
+    </div>
+  );
 }
