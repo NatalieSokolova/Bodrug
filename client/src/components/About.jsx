24af2bcf80cd26db99af76922b3a09d1fb58ce65
NatalieSokolova <nataliesklv@gmail.com>
2020-10-25 03:37:54 +0000

    adds photos to about page

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,10 +1,93 @@
 import React from "react";
+import { Card } from "antd";
+import "./About.css";
 
 export default function About({}) {
   return (
-    <div>
-      <h1>Some facts</h1>
-      <h1>Mission Statement goes here</h1>
+    <div class="about">
+      <div class="image-stack">
+        <div class="photo" id="one">
+          <img
+            src={require("../assets/about/IMG_1706.jpeg")}
+            alt="Yulia Bodrug"
+          />
+        </div>
+        <div class="photo" id="two">
+          <img
+            src={require("../assets/about/IMG_1711.jpg")}
+            alt="Yulia Bodrug"
+          />
+        </div>
+        <div class="photo" id="three">
+          <img
+            src={require("../assets/about/IMG_1710.jpeg")}
+            alt="Yulia Bodrug"
+          />
+        </div>
+        <div class="photo" id="four">
+          <img
+            src={require("../assets/about/IMG_1708.jpeg")}
+            alt="Yulia Bodrug"
+          />
+        </div>
+        <div class="photo" id="five">
+          <img
+            src={require("../assets/about/IMG_1709.jpg")}
+            alt="Yulia Bodrug"
+          />
+        </div>
+
+        <div class="photo" id="six">
+          <img
+            src={require("../assets/about/IMG_1712.jpg")}
+            alt="Yulia Bodrug"
+          />
+        </div>
+
+        <Card
+          class="about-card"
+          style={{
+            width: 300,
+            border: "2.5px solid black",
+            position: "absolute",
+            height: "40vh",
+            width: "40vh",
+            marginTop: "7.5em",
+            marginLeft: "60vw",
+          }}
+        >
+          {/* <div class="about-card"> */}
+          <h1 class="about-header">My Story</h1>
+          <p class="about-text">
+            I'm baby blog distillery try-hard ethical cold-pressed shabby chic
+            messenger bag meggings taiyaki occupy ennui. Heirloom artisan master
+            cleanse 3 wolf moon scenester prism forage art party chillwave
+            bespoke. Yuccie ugh you probably haven't heard of them bitters.
+            Franzen freegan meditation seitan succulents taxidermy. La croix
+            lo-fi marfa bespoke put a bird on it, tote bag hammock raw denim
+            direct trade intelligentsia neutra chillwave. Quinoa +1 lyft wolf
+            vaporware bicycle rights. Tbh tumblr gochujang swag, yuccie
+            succulents dreamcatcher small batch messenger bag farm-to-table
+            poutine craft beer copper mug chambray tote bag.
+          </p>
+          {/* </div> */}
+        </Card>
+        {/* <div class="about-card">
+          <h1 class="about-header">My Story</h1>
+          <p class="about-text">
+            I'm baby blog distillery try-hard ethical cold-pressed shabby chic
+            messenger bag meggings taiyaki occupy ennui. Heirloom artisan master
+            cleanse 3 wolf moon scenester prism forage art party chillwave
+            bespoke. Yuccie ugh you probably haven't heard of them bitters.
+            Franzen freegan meditation seitan succulents taxidermy. La croix
+            lo-fi marfa bespoke put a bird on it, tote bag hammock raw denim
+            direct trade intelligentsia neutra chillwave. Quinoa +1 lyft wolf
+            vaporware bicycle rights. Tbh tumblr gochujang swag, yuccie
+            succulents dreamcatcher small batch messenger bag farm-to-table
+            poutine craft beer copper mug chambray tote bag.
+          </p>
+        </div> */}
+      </div>
     </div>
   );
 }
