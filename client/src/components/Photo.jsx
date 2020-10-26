2e458403d05317c0c022f47faffc5cbedab368f9
NatalieSokolova <nataliesklv@gmail.com>
2020-10-26 06:10:30 +0000

    goes back to previous version

--- client/src/components/Photo.jsx
+++ client/src/components/Photo.jsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import usePhotoData from "../hooks/usePhotoData";
 import "./Photo.css";
 
 export default function Photo({}) {
   const { state, setState } = usePhotoData();
-  // let url;
 
   const photoList = state.photos.map((photo) => (
     <div key={photo.id} className="photoContainer">
@@ -20,10 +19,5 @@ export default function Photo({}) {
     </div>
   ));
 
-  return (
-    <span>
-      <h1>Photo Portfolio</h1>
-      <div>{photoList}</div>
-    </span>
-  );
+  return <div>{photoList}</div>;
 }
