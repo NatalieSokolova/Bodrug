7126309ec339c94d639dc5914e2cb517d6173526
NatalieSokolova <nataliesklv@gmail.com>
2020-10-28 22:46:31 +0000

    adds margins to photo

--- client/src/components/Faq.jsx
+++ client/src/components/Faq.jsx
@@ -5,24 +5,35 @@ import "./Faq.css";
 export default function Faq({}) {
   const { state, setState } = useFaqData();
 
-  const faqList = state.faqs.map((faq) => (
-    <div key={faq.id} className="faqContainer">
-      <div>
-        <b>{faq.question}</b>
-      </div>
-      <br />
-      <div>{faq.answer}</div>
-      <br />
-    </div>
-  ));
+  // const faqList = state.faqs.map((faq) => (
+  //   <div key={faq.id} className="faqContainer">
+  //     <div>
+  //       <b>{faq.question}</b>
+  //     </div>
+  //     <br />
+  //     <div>{faq.answer}</div>
+  //     <br />
+  //   </div>
+  // ));
 
   return (
     <div>
       <span>
-        <img src={require("../assets/IMG_1705.jpg")} />
+        <img src={require("../assets/IMG_1705.jpg")} className="photo" />
       </span>
       <span>
-        <div>{faqList}</div>
+        <div>
+          {state.faqs.map((faq) => (
+            <div key={faq.id} className="faqContainer">
+              <div>
+                <h1 className="question">{faq.question}</h1>
+              </div>
+              <br />
+              <p className="answer">{faq.answer}</p>
+              <br />
+            </div>
+          ))}
+        </div>
       </span>
     </div>
   );
