e644db78e95088d6b9a6848cadfdd8cc3dea415f
NatalieSokolova <nataliesklv@gmail.com>
2020-10-29 22:36:38 +0000

    applies the same styles to paintings

--- client/src/components/Painting.jsx
+++ client/src/components/Painting.jsx
@@ -4,29 +4,43 @@ import "./Painting.css";
 
 export default function Painting({}) {
   const { state, setState } = usePaintingData();
-  let url;
 
-  const paintingList = state.paintings.map((painting) => (
-    <div key={painting.id} className="paintingContainer">
-      <span>
-        <img
-          className="painting"
-          src={require(`../assets/paintings${painting.url}`)}
-          alt="painting By Yuliia Bodrug"
-        />
-      </span>
-      <div>CAD {painting.price}</div>
-      <div>year: {painting.year}</div>
-      <div>materials: {painting.materials}</div>
-      <div>{painting.description}</div>
-      <br />
-    </div>
-  ));
+  // const paintingList = state.paintings.map((painting) => (
+  //   <div key={painting.id} className="paintingContainer">
+  //     <span>
+  //       <img
+  //         className="painting"
+  //         src={require(`../assets/paintings${painting.url}`)}
+  //         alt="painting By Yuliia Bodrug"
+  //       />
+  //     </span>
+  //     <div>CAD {painting.price}</div>
+  //     <div>year: {painting.year}</div>
+  //     <div>materials: {painting.materials}</div>
+  //     <div>{painting.description}</div>
+  //     <br />
+  //   </div>
+  // ));
 
   return (
-    <span>
-      <h1>Painting Portfolio</h1>
-      <div>{paintingList}</div>
-    </span>
+    <div className="outsideContainer">
+      {state.paintings.map((painting) => (
+        <div key={painting.id} className="paintingContainer">
+          <span>
+            <img
+              className="painting"
+              src={require(`../assets/paintings${painting.url}`)}
+              alt="painting By Yuliia Bodrug"
+            />
+          </span>
+          <div className="description">
+            <div>CAD {painting.price}</div>
+            <div>year: {painting.year}</div>
+            <div>materials: {painting.materials}</div>
+            <div>{painting.description}</div>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
--- client/src/components/Photo.jsx
+++ client/src/components/Photo.jsx
@@ -5,19 +5,17 @@ import "./Photo.css";
 export default function Portfolio({}) {
   const { state, setState } = usePhotoData();
   return (
-    <span>
-      <div className="outsideContainer">
-        {state.photos.map((photo) => (
-          <div key={photo.id} className="photoContainer">
-            <img
-              className="photo"
-              src={require(`../assets/photos${photo.url}`)}
-              alt="Photo By Yuliia Bodrug"
-            />
-            <div className="description">{photo.description}</div>
-          </div>
-        ))}
-      </div>
-    </span>
+    <div className="outsideContainer">
+      {state.photos.map((photo) => (
+        <div key={photo.id} className="photoContainer">
+          <img
+            className="photo"
+            src={require(`../assets/photos${photo.url}`)}
+            alt="Photo By Yuliia Bodrug"
+          />
+          <div className="description">{photo.description}</div>
+        </div>
+      ))}
+    </div>
   );
 }
