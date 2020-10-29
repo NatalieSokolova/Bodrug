663b146a5af3f490f60f71568166d553077fbe03
NatalieSokolova <nataliesklv@gmail.com>
2020-10-29 01:29:22 +0000

    adds border to the email-form

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -14,7 +14,7 @@ export default function Contact({}) {
         <h1>social media?</h1>
         <h1>something else</h1>
       </div>
-      <div>
+      <div className="contact-photo">
         <img
           src={require("../assets/about/IMG_1710.jpeg")}
           alt="Yulia Bodrug"
