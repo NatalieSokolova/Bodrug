9523de25e7c3eca5c40aba8ca53bf998b7ebe321
NatalieSokolova <nataliesklv@gmail.com>
2020-10-29 00:56:12 +0000

    adds text columns

--- client/src/components/Pricing.jsx
+++ client/src/components/Pricing.jsx
@@ -5,6 +5,44 @@ export default function Pricing({}) {
   return (
     <div>
       <img src={require("../assets/top.jpg")} className="banner" />
+      <div className="pricing-text">
+        <div className="pricing-column">
+          <span>Dummy Text</span>
+          <p>
+            Cliche adaptogen ut cillum gentrify man braid street art. Fashion
+            axe raclette semiotics pug. Try-hard pour-over letterpress irony
+            shabby chic, shoreditch dreamcatcher biodiesel. Street art echo park
+            ennui cliche 8-bit prism. In letterpress in locavore bushwick.
+            Biodiesel la croix meh glossier, enamel pin stumptown nisi hexagon
+            proident vinyl viral roof party woke actually. Poke excepteur enim
+            in.
+          </p>
+        </div>
+        <div className="pricing-column">
+          <span>Dummy Text</span>
+          <p>
+            Cliche adaptogen ut cillum gentrify man braid street art. Fashion
+            axe raclette semiotics pug. Try-hard pour-over letterpress irony
+            shabby chic, shoreditch dreamcatcher biodiesel. Street art echo park
+            ennui cliche 8-bit prism. In letterpress in locavore bushwick.
+            Biodiesel la croix meh glossier, enamel pin stumptown nisi hexagon
+            proident vinyl viral roof party woke actually. Poke excepteur enim
+            in.
+          </p>
+        </div>
+        <div className="pricing-column">
+          <span>Dummy Text</span>
+          <p>
+            Cliche adaptogen ut cillum gentrify man braid street art. Fashion
+            axe raclette semiotics pug. Try-hard pour-over letterpress irony
+            shabby chic, shoreditch dreamcatcher biodiesel. Street art echo park
+            ennui cliche 8-bit prism. In letterpress in locavore bushwick.
+            Biodiesel la croix meh glossier, enamel pin stumptown nisi hexagon
+            proident vinyl viral roof party woke actually. Poke excepteur enim
+            in.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
