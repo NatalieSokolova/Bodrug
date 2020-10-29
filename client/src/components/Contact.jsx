a88eb9c141f875fdb2e6f1df169d8ece39d8cee7
NatalieSokolova <nataliesklv@gmail.com>
2020-10-29 01:25:27 +0000

    positions elements on the page

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import Message from "./Message";
+import "./Contact.css";
 
 export default function Contact({}) {
   return (
-    <div>
-      <h1>email me:</h1>
-      <div>
-        <Message />
+    <div class="contact-content">
+      <div className="contacts">
+        <h1>Don't hesitate to reach out!</h1>
+        <div>
+          <Message className="email-form" />
+        </div>
+        <h1>phone??</h1>
+        <h1>social media?</h1>
+        <h1>something else</h1>
       </div>
-      <h1>phone??</h1>
-      <h1>social media?</h1>
-      <h1>something else</h1>
+      <div>
+        <img
+          src={require("../assets/about/IMG_1710.jpeg")}
+          alt="Yulia Bodrug"
+        />
+      </div>
     </div>
   );
 }
