01cf0510af0a9678b319fe692e639aa202d9eb56
NatalieSokolova <nataliesklv@gmail.com>
2020-10-29 04:30:18 +0000

    positions text

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -10,49 +10,55 @@ export default function About({}) {
           alt="Yulia Bodrug"
         />
       </div>
-      <span class="about-header">My Story</span>
-      <p class="about-text">
-        I'm baby blog distillery try-hard ethical cold-pressed shabby chic
-        messenger bag meggings taiyaki occupy ennui. Heirloom artisan master
-        cleanse 3 wolf moon scenester prism forage art party chillwave bespoke.
-        Yuccie ugh you probably haven't heard of them bitters. Franzen freegan
-        meditation seitan succulents taxidermy. La croix lo-fi marfa bespoke put
-        a bird on it, tote bag hammock raw denim direct trade intelligentsia
-        neutra chillwave. Quinoa +1 lyft wolf vaporware bicycle rights. Tbh
-        tumblr gochujang swag, yuccie succulents dreamcatcher small batch
-        messenger bag farm-to-table poutine craft beer copper mug chambray tote
-        bag.
-      </p>
+      <div className="column">
+        <span class="about-header">My Story</span>
+        <p class="about-text">
+          I'm baby blog distillery try-hard ethical cold-pressed shabby chic
+          messenger bag meggings taiyaki occupy ennui. Heirloom artisan master
+          cleanse 3 wolf moon scenester prism forage art party chillwave
+          bespoke. Yuccie ugh you probably haven't heard of them bitters.
+          Franzen freegan meditation seitan succulents taxidermy. La croix lo-fi
+          marfa bespoke put a bird on it, tote bag hammock raw denim direct
+          trade intelligentsia neutra chillwave. Quinoa +1 lyft wolf vaporware
+          bicycle rights. Tbh tumblr gochujang swag, yuccie succulents
+          dreamcatcher small batch messenger bag farm-to-table poutine craft
+          beer copper mug chambray tote bag.
+        </p>
+      </div>
       <div class="photo">
         <img
           src={require("../assets/paintings/tamara-menzi-n-vnWQmmVoY-unsplash.jpg")}
           alt="Yulia Bodrug"
         />
       </div>
-      <span class="about-header">My Mission</span>
-      <p class="about-text">
-        I'm baby blog distillery try-hard ethical cold-pressed shabby chic
-        messenger bag meggings taiyaki occupy ennui. Heirloom artisan master
-        cleanse 3 wolf moon scenester prism forage art party chillwave bespoke.
-        Yuccie ugh you probably haven't heard of them bitters. Franzen freegan
-        meditation seitan succulents taxidermy. La croix lo-fi marfa bespoke put
-        a bird on it, tote bag hammock raw denim direct trade intelligentsia
-        neutra chillwave.
-      </p>
+      <div className="column">
+        <span class="about-header">My Mission</span>
+        <p class="about-text">
+          I'm baby blog distillery try-hard ethical cold-pressed shabby chic
+          messenger bag meggings taiyaki occupy ennui. Heirloom artisan master
+          cleanse 3 wolf moon scenester prism forage art party chillwave
+          bespoke. Yuccie ugh you probably haven't heard of them bitters.
+          Franzen freegan meditation seitan succulents taxidermy. La croix lo-fi
+          marfa bespoke put a bird on it, tote bag hammock raw denim direct
+          trade intelligentsia neutra chillwave.
+        </p>
+      </div>
       <div class="photo">
         <img
           src={require("../assets/paintings/the-new-york-public-library-R5oumqz_lhY-unsplash.jpg")}
           alt="Yulia Bodrug"
         />
       </div>
-      <span class="about-header">My Vision</span>
-      <p class="about-text">
-        I'm baby blog distillery try-hard ethical cold-pressed shabby chic
-        messenger bag meggings taiyaki occupy ennui. Heirloom artisan master
-        cleanse 3 wolf moon scenester prism forage art party chillwave bespoke.
-        Yuccie ugh you probably haven't heard of them bitters. Franzen freegan
-        meditation seitan succulents taxidermy.
-      </p>
+      <div className="column">
+        <span class="about-header">My Vision</span>
+        <p class="about-text">
+          I'm baby blog distillery try-hard ethical cold-pressed shabby chic
+          messenger bag meggings taiyaki occupy ennui. Heirloom artisan master
+          cleanse 3 wolf moon scenester prism forage art party chillwave
+          bespoke. Yuccie ugh you probably haven't heard of them bitters.
+          Franzen freegan meditation seitan succulents taxidermy.
+        </p>
+      </div>
       <div class="photo">
         <img
           src={require("../assets/paintings/europeana-qi9jveT9X6A-unsplash.jpg")}
