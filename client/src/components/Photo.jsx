3ca7e9adfee01b5f410a6a73c1c4c2a2ac12ad9d
NatalieSokolova <nataliesklv@gmail.com>
2020-10-30 22:41:40 +0000

    adds lightbox

--- client/src/components/Photo.jsx
+++ client/src/components/Photo.jsx
@@ -1,24 +1,43 @@
-import React, { useState, useEffect } from "react";
-import usePhotoData from "../hooks/usePhotoData";
-import { Card } from "antd";
-import "./Photo.css";
+// import React, { useState, useEffect } from "react";
+// import usePhotoData from "../hooks/usePhotoData";
+// import { Card } from "antd";
+// import "./Photo.css";
 
-export default function Portfolio() {
-  const { state, setState } = usePhotoData();
-  return (
-    <div className="outsideContainer">
-      {state.photos.map((photo) => (
-        <div key={photo.id} className="photoContainer">
-          <img
-            className="photo"
-            src={require(`../assets/photos${photo.url}`)}
-            alt="Photo By Yuliia Bodrug"
-          />
-          <Card className="photo-description-card">
-            <div>{photo.description}</div>
-          </Card>
-        </div>
-      ))}
-    </div>
-  );
-}
+// export default function Portfolio() {
+//   const { state, setState } = usePhotoData();
+//   return (
+//     <div className="outsideContainer">
+//       {state.photos.map((photo) => (
+//         <div key={photo.id} className="photoContainer">
+//           <img
+//             className="photo"
+//             src={require(`../assets/photos${photo.url}`)}
+//             alt="Photo By Yuliia Bodrug"
+//           />
+//           <Card className="photo-description-card">
+//             <div>{photo.description}</div>
+//           </Card>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+import React from "react";
+import Lightbox from "react-lightbox-component";
+
+const Photo = () => (
+  <div>
+    <Lightbox
+      images={[
+        {
+          src: require("../assets/photos/1-min.jpg"),
+          title: "image title",
+          description: "image description",
+        },
+      ]}
+    />
+  </div>
+);
+
+export default Photo;
