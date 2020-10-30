f0d1e6400b27f2acb08e1ba7d70b62c8a58523e5
NatalieSokolova <nataliesklv@gmail.com>
2020-10-30 23:50:43 +0000

    applies styling

--- client/src/components/Photo.jsx
+++ client/src/components/Photo.jsx
@@ -14,9 +14,9 @@
 //             src={require(`../assets/photos${photo.url}`)}
 //             alt="Photo By Yuliia Bodrug"
 //           />
-//           <Card className="photo-description-card">
-//             <div>{photo.description}</div>
-//           </Card>
+// <Card className="photo-description-card">
+//   <div>{photo.description}</div>
+// </Card>
 //         </div>
 //       ))}
 //     </div>
@@ -25,25 +25,30 @@
 
 import React, { useState, useEffect } from "react";
 import usePhotoData from "../hooks/usePhotoData";
-import { Card } from "antd";
 import { SRLWrapper } from "simple-react-lightbox";
+// import { Card } from "antd";
+import "./Photo.css";
 
 export default function Portfolio() {
   const { state, setState } = usePhotoData();
 
   return (
-    <div className="MyComponent">
+    <div className="outsideContainer">
       <SRLWrapper>
         {!state.photos ? (
           <span>Loading...</span>
         ) : (
           <div className="row">
             {state.photos.map((photo) => (
-              <div key={photo.id}>
+              <div key={photo.id} className="photoContainer">
                 <img
                   src={require(`../assets/photos${photo.url}`)}
                   alt={photo.description}
+                  className="photo"
                 />
+                {/* <Card className="photo-description-card">
+                  <div>{photo.description}</div>
+                </Card> */}
               </div>
             ))}
           </div>
