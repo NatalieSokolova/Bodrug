0ef0f4302dde32f0b893a73589fff54d673badc7
NatalieSokolova <nataliesklv@gmail.com>
2020-10-30 23:57:01 +0000

    removes description card on hover

--- client/src/components/Photo.jsx
+++ client/src/components/Photo.jsx
@@ -1,32 +1,6 @@
-// import React, { useState, useEffect } from "react";
-// import usePhotoData from "../hooks/usePhotoData";
-// import { Card } from "antd";
-// import "./Photo.css";
-
-// export default function Portfolio() {
-//   const { state, setState } = usePhotoData();
-//   return (
-//     <div className="outsideContainer">
-//       {state.photos.map((photo) => (
-//         <div key={photo.id} className="photoContainer">
-//           <img
-//             className="photo"
-//             src={require(`../assets/photos${photo.url}`)}
-//             alt="Photo By Yuliia Bodrug"
-//           />
-// <Card className="photo-description-card">
-//   <div>{photo.description}</div>
-// </Card>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-import React, { useState, useEffect } from "react";
+import React from "react";
 import usePhotoData from "../hooks/usePhotoData";
 import { SRLWrapper } from "simple-react-lightbox";
-// import { Card } from "antd";
 import "./Photo.css";
 
 export default function Portfolio() {
@@ -34,26 +8,25 @@ export default function Portfolio() {
 
   return (
     <div className="outsideContainer">
-      <SRLWrapper>
-        {!state.photos ? (
-          <span>Loading...</span>
-        ) : (
-          <div className="row">
-            {state.photos.map((photo) => (
-              <div key={photo.id} className="photoContainer">
-                <img
-                  src={require(`../assets/photos${photo.url}`)}
-                  alt={photo.description}
-                  className="photo"
-                />
-                {/* <Card className="photo-description-card">
-                  <div>{photo.description}</div>
-                </Card> */}
-              </div>
-            ))}
-          </div>
-        )}
-      </SRLWrapper>
+      <a href="#">
+        <SRLWrapper>
+          {!state.photos ? (
+            <span>Loading...</span>
+          ) : (
+            <div className="row">
+              {state.photos.map((photo) => (
+                <div key={photo.id} className="photoContainer">
+                  <img
+                    src={require(`../assets/photos${photo.url}`)}
+                    alt={photo.description}
+                    className="photo"
+                  />
+                </div>
+              ))}
+            </div>
+          )}
+        </SRLWrapper>
+      </a>
     </div>
   );
 }
