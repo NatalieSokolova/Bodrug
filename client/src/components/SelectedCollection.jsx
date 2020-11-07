0ed94ff4491e46e3a33b22871e9279e5ac2e6562
NatalieSokolova <nataliesklv@gmail.com>
2020-11-07 06:26:39 +0000

    renders photos by collection id onClick (for now under all collections => will be fixed later)

--- client/src/components/SelectedCollection.jsx
+++ client/src/components/SelectedCollection.jsx
@@ -1,26 +1,42 @@
-import React from "react";
-import useCollectionPhotoData from "../hooks/useCollectionPhotoData";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import { toast } from "react-toastify";
 import { copyrightError } from "../partials";
 import "./SelectedCollection.css";
 
-export default function SelectedCollection() {
+export default function SelectedCollection(props) {
   toast.configure();
 
-  const { state } = useCollectionPhotoData();
-  console.log("STATE: ", state);
+  const [state, setState] = useState({
+    photoList: [],
+  });
+
+  console.log("PROPS: ", props);
+
+  const getPhotosByCollectionId = useEffect(() => {
+    axios({
+      method: "GET",
+      url: `/collections/${props.id}/photos`,
+    })
+      .then((result) => {
+        setState((prev) => ({ ...prev, photoList: result.data }));
+      })
+      .catch((err) => console.log(err));
+  }, [props.id]);
+
+  const renderedPhotoList = state.photoList;
 
   return (
     <div className="collection">
-      {state.collectionPhotos.map((collectionPhoto) => (
-        <div key={collectionPhoto.id}>
+      {renderedPhotoList.map((renderedPhoto) => (
+        <div key={renderedPhoto.id}>
           <p>DESCRIPTION</p>
-          {collectionPhoto.description}
+          {renderedPhoto.description}
           <div>
             <img
-              src={require(`../assets/photos${collectionPhoto.url}`)}
-              alt={collectionPhoto.description}
+              src={require(`../assets/photos${renderedPhoto.url}`)}
+              alt={renderedPhoto.description}
               onContextMenu={(e) => {
                 copyrightError();
                 e.preventDefault();
