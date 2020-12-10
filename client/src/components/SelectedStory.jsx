bb650821454e5d06b175e8e0a60ed92c4ddc2f1d
NatalieSokolova <nataliesklv@gmail.com>
2020-12-10 04:02:08 +0000

    adds carousel

--- client/src/components/SelectedStory.jsx
+++ client/src/components/SelectedStory.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import { Carousel } from "antd";
 import { toast } from "react-toastify";
 import { copyrightError } from "../partials";
 import "./SelectedStory.css";
@@ -11,6 +11,23 @@ export default function SelectedStory(props) {
   const [state, setState] = useState({
     photoList: [],
   });
+
+  const contentStyle = {
+    // width: "40vw",
+    height: "100%",
+    width: "80vw",
+    color: "#fff",
+    lineHeight: "160px",
+    textAlign: "center",
+    margin: "0",
+  };
+
+  const [dotPosition, setDotPosition] = React.useState("top");
+  const PositionCarouselDemo = () => {
+    const handlePositionChange = ({ target: { value } }) => {
+      setDotPosition(value);
+    };
+  };
 
   const getPhotosByStoryId = useEffect(() => {
     axios({
@@ -26,24 +43,23 @@ export default function SelectedStory(props) {
   const renderedPhotoList = state.photoList;
 
   return (
-    <div className="story">
+    <Carousel effect="fade" dotPosition={dotPosition}>
       {renderedPhotoList.map((renderedPhoto) => (
         <div key={renderedPhoto.id}>
           <p className="photo-description">{renderedPhoto.description}</p>
 
-          <div>
-            <img
-              className="story-img"
-              src={require(`../assets/photos${renderedPhoto.url}`)}
-              alt={renderedPhoto.description}
-              onContextMenu={(e) => {
-                copyrightError();
-                e.preventDefault();
-              }}
-            />
-          </div>
+          <img
+            style={contentStyle}
+            className="story-img"
+            src={require(`../assets/photos${renderedPhoto.url}`)}
+            alt={renderedPhoto.description}
+            onContextMenu={(e) => {
+              copyrightError();
+              e.preventDefault();
+            }}
+          />
         </div>
       ))}
-    </div>
+    </Carousel>
   );
 }
--- client/src/components/Story.jsx
+++ client/src/components/Story.jsx
@@ -2,13 +2,11 @@ import React, { useState } from "react";
 import SelectedStory from "./SelectedStory";
 import useStoryData from "../hooks/useStoryData";
 import { Card } from "antd";
-
 import { toast } from "react-toastify";
 import { copyrightError } from "../partials";
 import "./Story.css";
 
 const { Meta } = Card;
-// const idList = [1, 2, 3, 4];
 
 export default function Story() {
   toast.configure();
@@ -22,7 +20,6 @@ export default function Story() {
       {state.stories.map((story) => (
         <div className="story-tile" key={story.id}>
           <Card
-            // hoverable
             cover={
               <img
                 src={require(`../assets/photos${story.coverurl}`)}
@@ -38,11 +35,11 @@ export default function Story() {
               />
             }
           >
-            {showPhotos && story.id === id ? <SelectedStory id={id} /> : null}
             <div className="story-description">
               <Meta title="Story Name" description={story.description} />
             </div>
           </Card>
+          {showPhotos && story.id === id ? <SelectedStory id={id} /> : null}
         </div>
       ))}
     </div>
