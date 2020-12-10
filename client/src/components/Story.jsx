283b2493e3ef3c1af37379285fe193e96e39896f
NatalieSokolova <nataliesklv@gmail.com>
2020-12-10 03:12:44 +0000

    adds card layout to stories page

--- client/src/components/Story.jsx
+++ client/src/components/Story.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import SelectedStory from "./SelectedStory";
 import useStoryData from "../hooks/useStoryData";
+import { Card } from "antd";
 
 import { toast } from "react-toastify";
 import { copyrightError } from "../partials";
 import "./Story.css";
 
-const idList = [1, 2, 3, 4];
+const { Meta } = Card;
+// const idList = [1, 2, 3, 4];
 
 export default function Story() {
   toast.configure();
@@ -19,23 +21,28 @@ export default function Story() {
     <div>
       {state.stories.map((story) => (
         <div className="story-tile" key={story.id}>
-          <img
-            src={require(`../assets/photos${story.coverurl}`)}
-            alt={story.description}
-            onContextMenu={(e) => {
-              copyrightError();
-              e.preventDefault();
-            }}
-            onClick={() => {
-              setShowPhotos(!showPhotos);
-              setId(story.id);
-            }}
-          />
-          {showPhotos && story.id === id ? <SelectedStory id={id} /> : null}
-
-          <div className="story-description">
-            <p>{story.description}</p>
-          </div>
+          <Card
+            // hoverable
+            cover={
+              <img
+                src={require(`../assets/photos${story.coverurl}`)}
+                alt={story.description}
+                onContextMenu={(e) => {
+                  copyrightError();
+                  e.preventDefault();
+                }}
+                onClick={() => {
+                  setShowPhotos(!showPhotos);
+                  setId(story.id);
+                }}
+              />
+            }
+          >
+            {showPhotos && story.id === id ? <SelectedStory id={id} /> : null}
+            <div className="story-description">
+              <Meta title="Story Name" description={story.description} />
+            </div>
+          </Card>
         </div>
       ))}
     </div>
