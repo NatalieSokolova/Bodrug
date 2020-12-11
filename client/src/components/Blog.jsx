effb1626e14d85cd8d7299d5ede9fe8321edffbf
NatalieSokolova <nataliesklv@gmail.com>
2020-12-11 03:28:48 +0000

    positions blog covers in a grid

--- client/src/components/Blog.jsx
+++ client/src/components/Blog.jsx
@@ -20,49 +20,51 @@ export default function Blog() {
         <h1 className="new-post-title">New Post Title</h1>
       </div>
 
-      <div className="old-posts">
-        <img
-          src={require("../assets/art/europeana-qi9jveT9X6A-unsplash.jpg")}
-          alt="Yulia Bodrug"
-          onContextMenu={(e) => {
-            copyrightError();
-            e.preventDefault();
-          }}
-        />
-        <h1 className="old-post-title">Old Post Title</h1>
-      </div>
-      <div className="old-posts">
-        <img
-          src={require("../assets/art/sergio-souza-gOzD9CZTwXw-unsplash.jpg")}
-          alt="Yulia Bodrug"
-          onContextMenu={(e) => {
-            copyrightError();
-            e.preventDefault();
-          }}
-        />
-        <h1 className="old-post-title">Old Post Title</h1>
-      </div>
-      <div className="old-posts">
-        <img
-          src={require("../assets/photos/19-min.jpg")}
-          alt="Yulia Bodrug"
-          onContextMenu={(e) => {
-            copyrightError();
-            e.preventDefault();
-          }}
-        />
-        <h1 className="old-post-title">Old Post Title</h1>
-      </div>
-      <div className="old-posts">
-        <img
-          src={require("../assets/photos/20-min.jpg")}
-          alt="Yulia Bodrug"
-          onContextMenu={(e) => {
-            copyrightError();
-            e.preventDefault();
-          }}
-        />
-        <h1 className="old-post-title">Old Post Title</h1>
+      <div className="old-post-list">
+        <div className="old-post">
+          <img
+            src={require("../assets/art/europeana-qi9jveT9X6A-unsplash.jpg")}
+            alt="Yulia Bodrug"
+            onContextMenu={(e) => {
+              copyrightError();
+              e.preventDefault();
+            }}
+          />
+          <h1 className="old-post-title">Old Post Title</h1>
+        </div>
+        <div className="old-post">
+          <img
+            src={require("../assets/art/sergio-souza-gOzD9CZTwXw-unsplash.jpg")}
+            alt="Yulia Bodrug"
+            onContextMenu={(e) => {
+              copyrightError();
+              e.preventDefault();
+            }}
+          />
+          <h1 className="old-post-title">Old Post Title</h1>
+        </div>
+        <div className="old-post">
+          <img
+            src={require("../assets/photos/19-min.jpg")}
+            alt="Yulia Bodrug"
+            onContextMenu={(e) => {
+              copyrightError();
+              e.preventDefault();
+            }}
+          />
+          <h1 className="old-post-title">Old Post Title</h1>
+        </div>
+        <div className="old-post">
+          <img
+            src={require("../assets/photos/20-min.jpg")}
+            alt="Yulia Bodrug"
+            onContextMenu={(e) => {
+              copyrightError();
+              e.preventDefault();
+            }}
+          />
+          <h1 className="old-post-title">Old Post Title</h1>
+        </div>
       </div>
     </div>
   );
