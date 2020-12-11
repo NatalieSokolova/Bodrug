7c66aafa4e3bcca6fec51532a8b03d49b60f74bc
NatalieSokolova <nataliesklv@gmail.com>
2020-12-11 03:08:58 +0000

    adds blod covers and titles

--- client/src/components/Blog.jsx
+++ client/src/components/Blog.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { copyrightError } from "../partials";
 import { toast } from "react-toastify";
+import "./Blog.css";
 
 export default function Blog() {
   toast.configure();
@@ -9,13 +10,59 @@ export default function Blog() {
     <div>
       <div className="new-post">
         <img
-          src={require("../assets/paintings/europeana-YIfFVwDcgu8-unsplash.jpg")}
+          src={require("../assets/art/falco-negenman-kdNs6NZMaD4-unsplash.jpg")}
           alt="Yulia Bodrug"
           onContextMenu={(e) => {
             copyrightError();
             e.preventDefault();
           }}
         />
+        <h1 className="new-post-title">New Post Title</h1>
+      </div>
+
+      <div className="old-posts">
+        <img
+          src={require("../assets/art/europeana-qi9jveT9X6A-unsplash.jpg")}
+          alt="Yulia Bodrug"
+          onContextMenu={(e) => {
+            copyrightError();
+            e.preventDefault();
+          }}
+        />
+        <h1 className="old-post-title">Old Post Title</h1>
+      </div>
+      <div className="old-posts">
+        <img
+          src={require("../assets/art/sergio-souza-gOzD9CZTwXw-unsplash.jpg")}
+          alt="Yulia Bodrug"
+          onContextMenu={(e) => {
+            copyrightError();
+            e.preventDefault();
+          }}
+        />
+        <h1 className="old-post-title">Old Post Title</h1>
+      </div>
+      <div className="old-posts">
+        <img
+          src={require("../assets/photos/19-min.jpg")}
+          alt="Yulia Bodrug"
+          onContextMenu={(e) => {
+            copyrightError();
+            e.preventDefault();
+          }}
+        />
+        <h1 className="old-post-title">Old Post Title</h1>
+      </div>
+      <div className="old-posts">
+        <img
+          src={require("../assets/photos/20-min.jpg")}
+          alt="Yulia Bodrug"
+          onContextMenu={(e) => {
+            copyrightError();
+            e.preventDefault();
+          }}
+        />
+        <h1 className="old-post-title">Old Post Title</h1>
       </div>
     </div>
   );
