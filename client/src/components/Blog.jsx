a305cd7fd3f5f05f87ac78133974891dfc128c4d
NatalieSokolova <nataliesklv@gmail.com>
2020-12-12 06:26:55 +0000

    styles blog list with data pulled from db

--- client/src/components/Blog.jsx
+++ client/src/components/Blog.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import useBlogEntryData from "../hooks/useBlogEntryData";
+
 import { copyrightError } from "../partials";
 import { toast } from "react-toastify";
 import "./Blog.css";
@@ -6,83 +8,45 @@ import "./Blog.css";
 export default function Blog() {
   toast.configure();
 
+  const { state } = useBlogEntryData();
+
   return (
     <div>
-      <div className="new-post">
-        <img
-          src={require("../assets/art/falco-negenman-kdNs6NZMaD4-unsplash.jpg")}
-          alt="Yulia Bodrug"
-          onContextMenu={(e) => {
-            copyrightError();
-            e.preventDefault();
-          }}
-        />
-        <h1 className="new-post-title">New Post Title</h1>
-        <h6 className="post-date">April 6, 2020</h6>
-        <hr />
-      </div>
-
+      {state.blogEntries.slice(0, 1).map((blogEntry) => (
+        <div className="new-post" key={1}>
+          <img
+            src={require(`../assets${blogEntry.coverurl}`)}
+            alt="Yulia Bodrug"
+            onContextMenu={(e) => {
+              copyrightError();
+              e.preventDefault();
+            }}
+          />
+          <h1 className="new-post-title">{blogEntry.title}</h1>
+          <h6 className="post-date">{blogEntry.datestring}</h6>
+          <hr />
+        </div>
+      ))}
       <div className="old-post-list">
-        <div className="old-post">
-          <img
-            src={require("../assets/art/europeana-qi9jveT9X6A-unsplash.jpg")}
-            alt="Yulia Bodrug"
-            onContextMenu={(e) => {
-              copyrightError();
-              e.preventDefault();
-            }}
-          />
-          <div className="title-container">
-            <h1 className="old-post-title">Old Post Title</h1>
-            <div class="vl"></div>
-            <h6 className="post-date">April 3, 2020</h6>
-          </div>
-        </div>
-        <div className="old-post">
-          <img
-            src={require("../assets/art/sergio-souza-gOzD9CZTwXw-unsplash.jpg")}
-            alt="Yulia Bodrug"
-            onContextMenu={(e) => {
-              copyrightError();
-              e.preventDefault();
-            }}
-          />
-          <div className="title-container">
-            <h1 className="old-post-title">Old Post Title</h1>
-            <div class="vl"></div>
-            <h6 className="post-date">April 3, 2020</h6>
-          </div>
-        </div>
-        <div className="old-post">
-          <img
-            src={require("../assets/photos/19-min.jpg")}
-            alt="Yulia Bodrug"
-            onContextMenu={(e) => {
-              copyrightError();
-              e.preventDefault();
-            }}
-          />
-          <div className="title-container">
-            <h1 className="old-post-title">Old Post Title</h1>
-            <div class="vl"></div>
-            <h6 className="post-date">May 26, 2020</h6>
-          </div>
-        </div>
-        <div className="old-post">
-          <img
-            src={require("../assets/photos/20-min.jpg")}
-            alt="Yulia Bodrug"
-            onContextMenu={(e) => {
-              copyrightError();
-              e.preventDefault();
-            }}
-          />
-          <div className="title-container">
-            <h1 className="old-post-title">Old Post Title</h1>
-            <div class="vl"></div>
-            <h6 className="post-date">June 6, 2020</h6>
-          </div>
-        </div>
+        {state.blogEntries
+          .slice(1, state.blogEntries.length)
+          .map((blogEntry) => (
+            <div className="old-post" key={blogEntry.id}>
+              <img
+                src={require(`../assets${blogEntry.coverurl}`)}
+                alt="Yulia Bodrug"
+                onContextMenu={(e) => {
+                  copyrightError();
+                  e.preventDefault();
+                }}
+              />
+              <div className="title-container">
+                <h1 className="old-post-title">{blogEntry.title}</h1>
+                <div class="vl"></div>
+                <h6 className="post-date">{blogEntry.datestring}</h6>
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
