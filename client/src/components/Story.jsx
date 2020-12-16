65ce7e68b116791fdca46ae53dfefde859d4d181
NatalieSokolova <nataliesklv@gmail.com>
2020-12-16 01:50:08 +0000

    adds carousel

--- client/src/components/Story.jsx
+++ client/src/components/Story.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import SelectedStory from "./SelectedStory";
 import useStoryData from "../hooks/useStoryData";
 import { toast } from "react-toastify";
+import { Carousel } from "antd";
 import { copyrightError } from "../partials";
 import "./Story.css";
 
@@ -13,32 +14,34 @@ export default function Story() {
   const { state } = useStoryData();
 
   return (
-    <div className="story-container">
-      {state.stories.map((story) => (
-        <div
-          className="story"
-          key={story.id}
-          onClick={() => {
-            setShowPhotos(true);
-            setId(story.id);
-          }}
-        >
-          <div className="story-description">
-            <h1 className="story-name">{story.name}</h1>
-            {story.description}
-          </div>
-
-          <img
-            src={require(`../assets/photos${story.coverurl}`)}
-            alt={story.description}
-            onContextMenu={(e) => {
-              copyrightError();
-              e.preventDefault();
+    <div>
+      <Carousel autoplay className="story-container">
+        {state.stories.map((story) => (
+          <div
+            className="story"
+            key={story.id}
+            onClick={() => {
+              setShowPhotos(true);
+              setId(story.id);
             }}
-          />
-          {showPhotos && story.id === id ? <SelectedStory id={id} /> : null}
-        </div>
-      ))}
+          >
+            <div className="story-description">
+              <h1 className="story-name">{story.name}</h1>
+              {story.description}
+            </div>
+
+            <img
+              src={require(`../assets/photos${story.coverurl}`)}
+              alt={story.description}
+              onContextMenu={(e) => {
+                copyrightError();
+                e.preventDefault();
+              }}
+            />
+            {showPhotos && story.id === id ? <SelectedStory id={id} /> : null}
+          </div>
+        ))}
+      </Carousel>
     </div>
   );
 }
