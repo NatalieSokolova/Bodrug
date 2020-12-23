8242a0e98e08a3baac998695b1cd535fa0d4d949
NatalieSokolova <nataliesklv@gmail.com>
2020-12-23 19:47:50 +0000

    adds admin-title

--- client/src/components/AdminDashboard.jsx
+++ client/src/components/AdminDashboard.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 import { Card } from "antd";
+import "./AdminDashboard.css";
 
 export default function AdminDashboard() {
-  const gridStyle = {
-    width: "30%",
-    textAlign: "center",
-  };
+  // const gridStyle = {
+  //   width: "30%",
+  //   textAlign: "center",
+  // };
 
   return (
     <div>
-      <Card title="Card Title">
-        <Card.Grid>Content</Card.Grid>
-        <Card.Grid>Content</Card.Grid>
-        <Card.Grid>Content</Card.Grid>
-        <Card.Grid>Content</Card.Grid>
-        <Card.Grid>Content</Card.Grid>
-        <Card.Grid>Content</Card.Grid>
+      <div className="admin-title">
+        <h1>Hey Iuliia!</h1>
+        <div>
+          Welcome to the admin panel - your place to add gorgeous content
+        </div>
+        <br />
+        <h2>Have a beautiful day!</h2>
+      </div>
+      <Card className="admin-panel">
+        <Card.Grid className="admin-card">All Photos</Card.Grid>
+        <Card.Grid className="admin-card">Collections</Card.Grid>
+        <Card.Grid className="admin-card">Stories</Card.Grid>
+        <Card.Grid className="admin-card">Art</Card.Grid>
+        <Card.Grid className="admin-card">FAQs</Card.Grid>
+        <Card.Grid className="admin-card">Blog</Card.Grid>
       </Card>
     </div>
   );
