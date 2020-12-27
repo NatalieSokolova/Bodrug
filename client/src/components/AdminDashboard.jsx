00d377e98eb0641aa891eac30611f12d68830ee1
NatalieSokolova <nataliesklv@gmail.com>
2020-12-27 20:18:34 +0000

    comments out conditional redirect in dashboard

--- client/src/components/AdminDashboard.jsx
+++ client/src/components/AdminDashboard.jsx
@@ -10,83 +10,83 @@ export default function AdminDashboard() {
   const [id, setId] = useState(null);
 
   return (
+    // <div>
+    //   {!auth ? (
+    //     <Redirect to="/login" />
+    //   ) : (
     <div>
-      {!auth ? (
-        <Redirect to="/login" />
-      ) : (
+      <div className="admin-title">
+        <h1>Hey Iuliia!</h1>
         <div>
-          <div className="admin-title">
-            <h1>Hey Iuliia!</h1>
-            <div>
-              Welcome to the admin panel - your place to add gorgeous content
-            </div>
-            <br />
-            <h2>Have a beautiful day!</h2>
-          </div>
-          <Card className="admin-panel">
-            <Card.Grid
-              onClick={() => {
-                setShowForm(true);
-                setId("allPhotos");
-              }}
-              className="admin-card"
-            >
-              All Photos
-            </Card.Grid>
-            <Card.Grid
-              onClick={() => {
-                setShowForm(true);
-                setId("collections");
-              }}
-              className="admin-card"
-            >
-              Collections
-            </Card.Grid>
-            <Card.Grid
-              onClick={() => {
-                setShowForm(true);
-                setId("stories");
-              }}
-              className="admin-card"
-            >
-              Stories
-            </Card.Grid>
-            <Card.Grid
-              onClick={() => {
-                setShowForm(true);
-                setId("art");
-              }}
-              className="admin-card"
-            >
-              Art
-            </Card.Grid>
-            <Card.Grid
-              onClick={() => {
-                setShowForm(true);
-                setId("faq");
-              }}
-              className="admin-card"
-            >
-              FAQs
-            </Card.Grid>
-            <Card.Grid
-              onClick={() => {
-                setShowForm(true);
-                setId("blog");
-              }}
-              className="admin-card"
-            >
-              Blog
-            </Card.Grid>
-          </Card>
-
-          {showForm && id === "faq" ? (
-            <div className="upload-container">
-              <FaqUpload />
-            </div>
-          ) : null}
+          Welcome to the admin panel - your place to add gorgeous content
         </div>
-      )}
+        <br />
+        <h2>Have a beautiful day!</h2>
+      </div>
+      <Card className="admin-panel">
+        <Card.Grid
+          onClick={() => {
+            setShowForm(true);
+            setId("allPhotos");
+          }}
+          className="admin-card"
+        >
+          All Photos
+        </Card.Grid>
+        <Card.Grid
+          onClick={() => {
+            setShowForm(true);
+            setId("collections");
+          }}
+          className="admin-card"
+        >
+          Collections
+        </Card.Grid>
+        <Card.Grid
+          onClick={() => {
+            setShowForm(true);
+            setId("stories");
+          }}
+          className="admin-card"
+        >
+          Stories
+        </Card.Grid>
+        <Card.Grid
+          onClick={() => {
+            setShowForm(true);
+            setId("art");
+          }}
+          className="admin-card"
+        >
+          Art
+        </Card.Grid>
+        <Card.Grid
+          onClick={() => {
+            setShowForm(true);
+            setId("faq");
+          }}
+          className="admin-card"
+        >
+          FAQs
+        </Card.Grid>
+        <Card.Grid
+          onClick={() => {
+            setShowForm(true);
+            setId("blog");
+          }}
+          className="admin-card"
+        >
+          Blog
+        </Card.Grid>
+      </Card>
+
+      {showForm && id === "faq" ? (
+        <div className="upload-container">
+          <FaqUpload />
+        </div>
+      ) : null}
     </div>
+    //   )}
+    // </div>
   );
 }
