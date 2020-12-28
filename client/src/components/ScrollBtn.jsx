b6a8f7a758d98e74d12ec796b23ace1ffc8213d6
NatalieSokolova <nataliesklv@gmail.com>
2020-12-28 22:05:46 +0000

    shows btn on scroll

--- client/src/components/ScrollBtn.jsx
+++ client/src/components/ScrollBtn.jsx
@@ -1,7 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./ScrollBtn.css";
 
 export default function ScrollBtn() {
   const [show, setShow] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setShow(true);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("scroll", () => toggleVisibility());
+  }, []);
+
   return <div>{show ? <button id="scroll-btn">scroll</button> : null}</div>;
 }
