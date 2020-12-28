125bece001dceea22fb4d58f1d237b360981555d
NatalieSokolova <nataliesklv@gmail.com>
2020-12-28 22:20:29 +0000

    scrolls to bottom by 750 px

--- client/src/components/ScrollBtn.jsx
+++ client/src/components/ScrollBtn.jsx
@@ -3,16 +3,32 @@ import "./ScrollBtn.css";
 
 export default function ScrollBtn() {
   const [show, setShow] = useState(false);
+  // const [position, setPosition] = useState("window.innerHeight");
 
   const toggleVisibility = () => {
-    if (window.pageYOffset > 300) {
+    if (window.pageYOffset > 150) {
       setShow(true);
     }
+  };
+
+  const handleScroll = () => {
+    window.scrollBy({
+      top: 750,
+      behavior: "smooth",
+    });
   };
 
   useEffect(() => {
     document.addEventListener("scroll", () => toggleVisibility());
   }, []);
 
-  return <div>{show ? <button id="scroll-btn">scroll</button> : null}</div>;
+  return (
+    <div>
+      {show ? (
+        <button id="scroll-btn" onClick={() => handleScroll()}>
+          scroll
+        </button>
+      ) : null}
+    </div>
+  );
 }
