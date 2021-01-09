65d419342cc76a9145d86a49e7d36cb1c6662c37
NatalieSokolova <nataliesklv@gmail.com>
2021-01-09 22:09:03 +0000

    wraps PhotoUpload with CloudinaryContext

--- client/src/components/PhotoUpload.jsx
+++ client/src/components/PhotoUpload.jsx
@@ -1,13 +1,33 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
+// import { cloudinary, openUploadWidget } from "cloudinary";
+import { CloudinaryContext } from "cloudinary-react";
 
 export default function PhotoUpload() {
   toast.configure();
 
+  const uploadWidget = () => {
+    cloudinary.openUploadWidget(
+      {
+        cloud_name: "nataliesklv",
+        upload_preset: "qtiu4svf",
+        tags: ["bodrug"],
+      },
+      function (error, result) {
+        console.log(result);
+      }
+    );
+  };
+
   return (
     <div>
-      <div>PHOTO UPLOAD</div>
+      <CloudinaryContext cloudName="nataliesklv">
+        <div>PHOTO UPLOAD</div>
+        <button onClick={openUploadWidget} className="upload-button">
+          Upload Image
+        </button>
+      </CloudinaryContext>
     </div>
   );
 }
