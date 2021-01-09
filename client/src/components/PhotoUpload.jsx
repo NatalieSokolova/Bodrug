78a45f13534ec400c58a78a16078e7595b7a56f1
NatalieSokolova <nataliesklv@gmail.com>
2021-01-09 22:20:20 +0000

    configures cloudinary service

--- client/src/components/PhotoUpload.jsx
+++ client/src/components/PhotoUpload.jsx
@@ -3,30 +3,49 @@ import axios from "axios";
 import { toast } from "react-toastify";
 // import { cloudinary, openUploadWidget } from "cloudinary";
 import { CloudinaryContext } from "cloudinary-react";
+import { fetchPhotos, openUploadWidget } from "../CloudinaryService";
 
 export default function PhotoUpload() {
   toast.configure();
 
-  const uploadWidget = () => {
-    cloudinary.openUploadWidget(
-      {
-        cloud_name: "nataliesklv",
-        upload_preset: "qtiu4svf",
-        tags: ["bodrug"],
-      },
-      function (error, result) {
-        console.log(result);
+  const [images, setImages] = useState([]);
+  // const uploadWidget = () => {
+  //   cloudinary.openUploadWidget(
+  //     {
+  //       cloud_name: "nataliesklv",
+  //       upload_preset: "qtiu4svf",
+  //       tags: ["bodrug"],
+  //     },
+  //     function (error, result) {
+  //       console.log(result);
+  //     }
+  //   );
+  // };
+
+  const beginUpload = (tag) => {
+    const uploadOptions = {
+      cloudName: "nataliesklv",
+      tags: ["bodrug"],
+      uploadPreset: "qtiu4svf",
+    };
+
+    openUploadWidget(uploadOptions, (error, photos) => {
+      if (!error) {
+        console.log(photos);
+        if (photos.event === "success") {
+          setImages([...images, photos.info.public_id]);
+        }
+      } else {
+        console.log(error);
       }
-    );
+    });
   };
 
   return (
-    <div>
+    <div className="upload-form">
       <CloudinaryContext cloudName="nataliesklv">
         <div>PHOTO UPLOAD</div>
-        <button onClick={openUploadWidget} className="upload-button">
-          Upload Image
-        </button>
+        <button onClick={() => beginUpload()}>Upload Image</button>
       </CloudinaryContext>
     </div>
   );
