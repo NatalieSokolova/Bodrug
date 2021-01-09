b7b1b42af2e1c1fd5d5204f3ebd390388628a0e7
NatalieSokolova <nataliesklv@gmail.com>
2021-01-09 23:15:27 +0000

    fetches cloudinary img url

--- client/src/components/PhotoUpload.jsx
+++ client/src/components/PhotoUpload.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { CloudinaryContext } from "cloudinary-react";
-import { openUploadWidget } from "../CloudinaryService";
+import { fetchPhotos, openUploadWidget } from "../CloudinaryService";
 import { notifyError, notifySuccess } from "../partials";
 
 export default function PhotoUpload() {
   toast.configure();
 
-  const [images, setImages] = useState([]);
+  // const [images, setImages] = useState([]);
 
   const beginUpload = (tag) => {
     const uploadOptions = {
@@ -21,10 +21,21 @@ export default function PhotoUpload() {
       if (!error) {
         console.log(photos);
         if (photos.event === "success") {
-          console.log("IMAGE UPLOADED!");
+          console.log("IMAGE UPLOADED: ", photos.info.url);
+
           notifySuccess("Woo-hoo! Image posted successfully!");
-          setImages([...images, photos.info.public_id]);
-          console.log("IMAGES: ", images);
+          // setImages([...images, photos.info.public_id]);
+          // axios
+          //   .post("http://localhost:3001/photos", {
+          //     url: `https://res.cloudinary.com/nataliesklv/image/upload/v1610232496/${}.jpg`
+          //   })
+          //   .then((response) => {
+          //     notifySuccess("Woo-hoo! FAQ posted successfully!");
+          //   })
+          //   .catch((error) => {
+          //     notifyError("OOPS! Something went wrong. Please, try again");
+          //     console.log("TROUBLE! ", error);
+          //   });
         }
       } else {
         notifyError("OOPS! Something went wrong. Please, try again");
@@ -33,8 +44,13 @@ export default function PhotoUpload() {
     });
   };
 
+  // useEffect(() => {
+  //   fetchPhotos("bodrug", setImages);
+  // }, []);
+
   return (
     <div className="upload-form">
+      {/* {console.log("IMAGES: ", images)} */}
       <CloudinaryContext cloudName="nataliesklv">
         <div>PHOTO UPLOAD</div>
         <button onClick={() => beginUpload("bodrug")}>Upload Image</button>
@@ -42,3 +58,8 @@ export default function PhotoUpload() {
     </div>
   );
 }
+
+// ["aqyngsip5usfqngidh2y"]
+// ["fdzdku3mc2a1tvftscfj"]
+// v1610232496
+// v1610232496
