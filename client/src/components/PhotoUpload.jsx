d4241cdbda67b37cfb633ce1b874b08b002f46c8
NatalieSokolova <nataliesklv@gmail.com>
2021-01-09 23:36:11 +0000

    makes a post request to the db with a url

--- client/src/components/PhotoUpload.jsx
+++ client/src/components/PhotoUpload.jsx
@@ -8,8 +8,6 @@ import { notifyError, notifySuccess } from "../partials";
 export default function PhotoUpload() {
   toast.configure();
 
-  // const [images, setImages] = useState([]);
-
   const beginUpload = (tag) => {
     const uploadOptions = {
       cloudName: "nataliesklv",
@@ -20,22 +18,25 @@ export default function PhotoUpload() {
     openUploadWidget(uploadOptions, (error, photos) => {
       if (!error) {
         console.log(photos);
-        if (photos.event === "success") {
-          console.log("IMAGE UPLOADED: ", photos.info.url);
 
-          notifySuccess("Woo-hoo! Image posted successfully!");
-          // setImages([...images, photos.info.public_id]);
-          // axios
-          //   .post("http://localhost:3001/photos", {
-          //     url: `https://res.cloudinary.com/nataliesklv/image/upload/v1610232496/${}.jpg`
-          //   })
-          //   .then((response) => {
-          //     notifySuccess("Woo-hoo! FAQ posted successfully!");
-          //   })
-          //   .catch((error) => {
-          //     notifyError("OOPS! Something went wrong. Please, try again");
-          //     console.log("TROUBLE! ", error);
-          //   });
+        const newPhoto = {
+          url: photos.info.url,
+        };
+
+        if (photos.event === "success") {
+          console.log("IMAGE URL: ", photos.info.url);
+
+          // also needs a delete request when img is deselected!!!
+          axios
+            .post("http://localhost:3001/photos", newPhoto)
+            .then((response) => {
+              console.log("SUCCESS! ", response);
+              notifySuccess("Woo-hoo! Image posted successfully!");
+            })
+            .catch((error) => {
+              notifyError("OOPS! Something went wrong. Please, try again");
+              console.log("TROUBLE! ", error);
+            });
         }
       } else {
         notifyError("OOPS! Something went wrong. Please, try again");
@@ -44,13 +45,8 @@ export default function PhotoUpload() {
     });
   };
 
-  // useEffect(() => {
-  //   fetchPhotos("bodrug", setImages);
-  // }, []);
-
   return (
     <div className="upload-form">
-      {/* {console.log("IMAGES: ", images)} */}
       <CloudinaryContext cloudName="nataliesklv">
         <div>PHOTO UPLOAD</div>
         <button onClick={() => beginUpload("bodrug")}>Upload Image</button>
@@ -58,8 +54,3 @@ export default function PhotoUpload() {
     </div>
   );
 }
-
-// ["aqyngsip5usfqngidh2y"]
-// ["fdzdku3mc2a1tvftscfj"]
-// v1610232496
-// v1610232496
