0805b84ddb87da72566276d491d891917e5094cb
NatalieSokolova <nataliesklv@gmail.com>
2021-01-10 00:42:53 +0000

    defines handleChange function

--- client/src/components/PhotoUpload.jsx
+++ client/src/components/PhotoUpload.jsx
@@ -51,23 +51,56 @@ export default function PhotoUpload() {
         if (photos.event === "success") {
           console.log("IMAGE URL: ", photos.info.url);
 
+          setPhoto({
+            ...photo,
+            url: photos.info.url,
+          });
+
           // also needs a delete request when img is deselected!!!
-          axios
-            .post("http://localhost:3001/photos", newPhoto)
-            .then((response) => {
-              console.log("SUCCESS! ", response);
-              notifySuccess("Woo-hoo! Image posted successfully!");
-            })
-            .catch((error) => {
-              notifyError("OOPS! Something went wrong. Please, try again");
-              console.log("TROUBLE! ", error);
-            });
+          // axios
+          //   .post("http://localhost:3001/photos", newPhoto)
+          //   .then((response) => {
+          //     console.log("SUCCESS! ", response);
+          //     notifySuccess("Woo-hoo! Image posted successfully!");
+          //   })
+          //   .catch((error) => {
+          //     notifyError("OOPS! Something went wrong. Please, try again");
+          //     console.log("TROUBLE! ", error);
+          //   });
         }
       } else {
         notifyError("OOPS! Something went wrong. Please, try again");
         console.log("ERROR: ", error);
       }
     });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const newPhoto = {
+      url: photo.url,
+      description: photo.description,
+      collection_id: photo.collection_id,
+      story_id: photo.story_id,
+    };
+
+    console.log("NEW: ", newPhoto);
+
+    if (newPhoto.url) {
+      axios
+        .post("http://localhost:3001/photos", newPhoto)
+        .then((response) => {
+          console.log("SUCCESS! ", response);
+          notifySuccess("Woo-hoo! Image posted successfully!");
+        })
+        .catch((error) => {
+          notifyError("OOPS! Something went wrong. Please, try again");
+          console.log("TROUBLE! ", error);
+        });
+    } else {
+      notifyError("You can't submit an empty FAQ. Please, try again");
+    }
   };
 
   return (
