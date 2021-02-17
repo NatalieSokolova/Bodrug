32bd6e66a4a4bd77cfe27ff1ccb77156e7206a94
Natalie Sokolova <nataliesklv@gmail.com>
2021-02-17 12:22:48 -0500

    adds an upload form

--- client/src/components/CollectionUpload.jsx
+++ client/src/components/CollectionUpload.jsx
@@ -1,10 +1,63 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { toast } from "react-toastify";
+import { notifyError, notifySuccess } from "../partials";
+import { Form, Input, Button } from "antd";
 
 export default function CollectionUpload() {
+  toast.configure();
+
+  const [form] = Form.useForm();
+
   return (
-    <div>
-      <div>NEW COLLECTION STUFF</div>
+    <div className="upload-container">
+      <h1>Add a new collection below</h1>
+      <Form
+        form={form}
+        className="upload-form"
+        name="basic"
+        initialValues={{ remember: true }}
+      >
+        <div
+          style={{
+            padding: "5vh 2vw",
+            marginBottom: "-7.5vh",
+          }}
+        >
+          <Form.Item label="name" name="name" rules={[{ required: false }]}>
+            <Input
+              name="name"
+              // value={photo.description}
+              // onChange={handleChange}
+            />
+          </Form.Item>
+          <Form.Item
+            label="description"
+            name="description"
+            rules={[{ required: false }]}
+          >
+            <Input.TextArea
+              name="description"
+              // value={photo.description}
+              // onChange={handleChange}
+            />
+          </Form.Item>
+          <Form.Item label="cover photo">
+            <div>COVER PHOTO</div>
+          </Form.Item>
+          <Form.Item label="photos">
+            <div>PHOTOS</div>
+          </Form.Item>
+        </div>
+
+        <Form.Item
+          style={{
+            marginBottom: "0",
+          }}
+        >
+          <Button className="btn btn-primary post-btn">POST</Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
