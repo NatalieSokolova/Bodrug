9468c6f0ba2bb23f8a83f78f6cc42ba30019f63f
Natalie Sokolova <nataliesklv@gmail.com>
2021-02-17 18:52:02 -0500

    requires name and description

--- client/src/components/CollectionUpload.jsx
+++ client/src/components/CollectionUpload.jsx
@@ -17,6 +17,7 @@ export default function CollectionUpload() {
   });
 
   const [photos, setPhotos] = useState([]);
+  const [image, setImage] = useState("cover");
 
   const handleChange = (event) => {
     const target = event.target;
@@ -63,6 +64,7 @@ export default function CollectionUpload() {
         .then((response) => {
           console.log("SUCCESS! ", response);
           notifySuccess("Woo-hoo! Collection created successfully!");
+          setImage("photos");
         })
         .catch((error) => {
           notifyError("OOPS! Something went wrong. Please, try again");
@@ -112,11 +114,19 @@ export default function CollectionUpload() {
       >
         <div
           style={{
-            padding: "5vh 2vw",
-            marginBottom: "-7.5vh",
+            paddingTop: "5vh",
           }}
         >
-          <Form.Item label="name" name="name" rules={[{ required: false }]}>
+          <Form.Item
+            label="name"
+            name="name"
+            rules={[
+              {
+                required: true,
+                message: "Please input the collection name!",
+              },
+            ]}
+          >
             <Input
               name="name"
               value={collection.name}
@@ -126,7 +136,12 @@ export default function CollectionUpload() {
           <Form.Item
             label="description"
             name="description"
-            rules={[{ required: false }]}
+            rules={[
+              {
+                required: true,
+                message: "Please input the collection description!",
+              },
+            ]}
           >
             <Input.TextArea
               name="description"
@@ -134,59 +149,68 @@ export default function CollectionUpload() {
               onChange={handleChange}
             />
           </Form.Item>
-          <Form.Item label="cover photo">
+          {image === "cover" ? (
             <div>
-              {state.photos.map((photo) => (
-                <div key={photo.id} className="photoContainer">
-                  <img
-                    src={photo.url}
-                    alt={photo.description}
-                    onClick={() => setCover(photo.url)}
-                    className="photo"
-                  />
+              <Form.Item label="cover photo">
+                <div>
+                  {state.photos.map((photo) => (
+                    <div key={photo.id} className="photoContainer">
+                      <img
+                        src={photo.url}
+                        alt={photo.description}
+                        onClick={() => setCover(photo.url)}
+                        className="photo"
+                      />
+                    </div>
+                  ))}
                 </div>
-              ))}
+              </Form.Item>
+
+              <Form.Item
+                style={{
+                  marginBottom: "0",
+                }}
+              >
+                <Button
+                  className="btn btn-primary post-btn"
+                  onClick={createCollection}
+                >
+                  CREATE COLLECTION
+                </Button>
+              </Form.Item>
             </div>
-          </Form.Item>
-
-          <Form.Item
-            style={{
-              marginBottom: "0",
-            }}
-          >
-            <Button
-              className="btn btn-primary post-btn"
-              onClick={createCollection}
-            >
-              CREATE COLLECTION
-            </Button>
-          </Form.Item>
-
-          <Form.Item label="photos">
+          ) : (
             <div>
-              {state.photos.map((photo) => (
-                <div key={photo.id} className="photoContainer">
-                  <img
-                    src={photo.url}
-                    alt={photo.description}
-                    onClick={() => setPhotoIds(photo.id)}
-                    className="photo"
-                  />
+              <Form.Item label="photos">
+                <div>
+                  {state.photos.map((photo) => (
+                    <div key={photo.id} className="photoContainer">
+                      <img
+                        src={photo.url}
+                        alt={photo.description}
+                        onClick={() => setPhotoIds(photo.id)}
+                        className="photo"
+                      />
+                    </div>
+                  ))}
                 </div>
-              ))}
+              </Form.Item>
+
+              <Form.Item
+                style={{
+                  marginBottom: "0",
+                }}
+              >
+                <Button
+                  className="btn btn-primary post-btn"
+                  onClick={addPhotos}
+                >
+                  ADD PHOTOS
+                </Button>
+              </Form.Item>
             </div>
-          </Form.Item>
+          )}
         </div>
-
-        <Form.Item
-          style={{
-            marginBottom: "0",
-          }}
-        >
-          <Button className="btn btn-primary post-btn" onClick={addPhotos}>
-            ADD PHOTOS
-          </Button>
-        </Form.Item>
       </Form>
     </div>
   );
