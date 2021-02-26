290791fe999059be12095b852d5e1911e3d5b131
Natalie Sokolova <nataliesklv@gmail.com>
2021-02-25 19:58:32 -0500

    hide faq upload form onclick of delete btn

--- client/src/components/FaqUpload.jsx
+++ client/src/components/FaqUpload.jsx
@@ -57,49 +57,61 @@ export default function FaqUpload({
 
   return (
     <div>
-      <h1>Add a new FAQ below</h1>
-      <Form
-        form={form}
-        className="upload-form"
-        name="basic"
-        initialValues={{ remember: true }}
-      >
-        <div className="formInput">
-          <Form.Item
-            label="question"
-            name="question"
-            rules={[{ required: true, message: "Please input your question!" }]}
+      {showDeleteContainer ? null : (
+        <div>
+          <h1>Add a new FAQ below</h1>
+          <Form
+            form={form}
+            className="upload-form"
+            name="basic"
+            initialValues={{ remember: true }}
           >
-            <Input.TextArea
-              name="question"
-              value={faq.question}
-              onChange={handleChange}
-            />
-          </Form.Item>
+            <div className="formInput">
+              <Form.Item
+                label="question"
+                name="question"
+                rules={[
+                  { required: true, message: "Please input your question!" },
+                ]}
+              >
+                <Input.TextArea
+                  name="question"
+                  value={faq.question}
+                  onChange={handleChange}
+                />
+              </Form.Item>
 
-          <Form.Item
-            label="answer"
-            name="answer"
-            rules={[{ required: true, message: "Please input your answer!" }]}
-          >
-            <Input.TextArea
-              value={faq.answer}
-              name="answer"
-              onChange={handleChange}
-            />
-          </Form.Item>
+              <Form.Item
+                label="answer"
+                name="answer"
+                rules={[
+                  { required: true, message: "Please input your answer!" },
+                ]}
+              >
+                <Input.TextArea
+                  value={faq.answer}
+                  name="answer"
+                  onChange={handleChange}
+                />
+              </Form.Item>
+            </div>
+
+            <Form.Item
+              style={{
+                marginBottom: "0",
+              }}
+            >
+              <Button
+                onClick={handleSubmit}
+                className="btn btn-primary post-btn"
+              >
+                POST
+              </Button>
+            </Form.Item>
+          </Form>
         </div>
+      )}
 
-        <Form.Item
-          style={{
-            marginBottom: "0",
-          }}
-        >
-          <Button onClick={handleSubmit} className="btn btn-primary post-btn">
-            POST
-          </Button>
-        </Form.Item>
-      </Form>
       <DeleteBtn
         showDeleteContainer={showDeleteContainer}
         setShowDeleteContainer={setShowDeleteContainer}
